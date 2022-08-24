896addc5b35d032938176243b892d30af866db54
Wilker Bruno <wilker.bruno@sccon.com.br>
2022-08-24 20:33:27 -0300

    feat: implementando OrderRepository.find

--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -1,8 +1,10 @@
 import Order from "../../../../domain/checkout/entity/order";
+import OrderItem from "../../../../domain/checkout/entity/order_item";
+import OrderRepositoryInterface from "../../../../domain/checkout/repository/order-repository.interface";
 import OrderItemModel from "./order-item.model";
 import OrderModel from "./order.model";
 
-export default class OrderRepository {
+export default class OrderRepository implements OrderRepositoryInterface {
   async create(entity: Order): Promise<void> {
     await OrderModel.create(
       {
@@ -22,4 +24,17 @@ export default class OrderRepository {
       }
     );
   }
+  update(entity: Order): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  async find(id: string): Promise<Order> {
+    const model = await OrderModel.findOne({where: { id }, include: ["items"]});
+    const items = model.items.map(o => new OrderItem(o.id, o.name, o.price, o.product_id, o.quantity));
+    
+    return new Order(model.id, model.customer_id, items);    
+  }
+  findAll(): Promise<Order[]> {
+    throw new Error("Method not implemented.");
+  }
+  
 }
